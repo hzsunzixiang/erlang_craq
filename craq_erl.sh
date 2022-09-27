15c7f5e6955db0bee9f623ea9837266e37fa2636
haichengsun <haichengsun123@163.com>
2022-09-27 03:21:52 -0400

    commit

--- craq_erl.sh
+++ craq_erl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /Users/gyanendraaggarwal/erlang/code/erlang_craq
-
+#cd /Users/gyanendraaggarwal/erlang/code/erlang_craq
+cd /home/ericksun/program/erlang_craq
 erl -sname $1 -pa ./ebin -pa ./craq_test/ebin -pa ./deps/lager/ebin -pa ./deps/goldrush/ebin -config ./sys
