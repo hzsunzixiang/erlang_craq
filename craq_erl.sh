add86730822fad0277126babb13960ab724deaa5
haichengsun <haichengsun123@163.com>
2022-10-21 05:20:57 -0400

    commit

--- craq_erl.sh
+++ craq_erl.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 #cd /home/ericksun/program/erlang_craq
-rebar3 shell --config config/sys.config --sname $1
+#rebar3 shell --config config/sys.config --sname $1
+rebar3 shell --sname $1
+
+
+
+# 在rebar.config中 有如下信息的是时候不用加 --config
+#        {sys_config, "./config/sys.config"},
+#        {vm_args, "./config/vm.args"}
+#
