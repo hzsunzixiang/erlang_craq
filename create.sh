706049697b07a92c781ada9976e7992ff98eff99
haichengsun <haichengsun123@163.com>
2022-11-21 01:52:24 -0500

    commit

--- create.sh
+++ create.sh
@@ -6,7 +6,7 @@ cscope -bq -i $DIR/cscope_source.files  -f cscope_source.out
 
 #ctags -R *.erl *.hrl
 
-FILE="$DIR/craq_test $DIR/src $DIR/include"
+FILE="$DIR/craq_test $DIR/src $DIR/include $DIR/trace_log"
 
 for i in $FILE
 do
