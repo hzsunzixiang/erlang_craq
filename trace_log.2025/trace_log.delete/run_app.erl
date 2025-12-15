91e7464cf409b75e8aee2e100047c6bb470e1e03
sunzixiang <haichengsun123@163.com>
2025-12-15 04:37:33 -0500

    delete

---
+++ trace_log.2025/trace_log.delete/run_app.erl
@@ -0,0 +1,17 @@
+N1 = 'ec_n1@rabbitmq4-1'.
+N2 = 'ec_n2@rabbitmq4-2'.
+N3 = 'ec_n3@rabbitmq4-3'.
+NodeList = [N1, N2, N3].
+erlang_craq:delete(N1, person, 10, [gender]).
+erlang_craq:delete(N1, person, 10).
+
+%erlang_craq:query(N1, person, 10).
+%rlang_craq:setup_repl(NodeList).
+%erlang_craq:update(N1, person, 10, [{name, john}, {age, 30}, {gender, male}]).
+%erlang_craq:update(N2, person, 10, [{age, 40}]).
+%erlang_craq:update(N3, person, 10, [{name, john_smith}], [age]).
+%erlang_craq:update(N1, [{candidate, 10, [{name,donald_trump},{party.republican}]}, {person, 10, [{name,john_smith},{age,40}]}]).
+
+
+
+
