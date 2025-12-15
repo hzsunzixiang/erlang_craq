0b16317f0e848ac071a216d80c9a6666165e382c
ericksun <haichengsun123@163.com>
2025-12-15 03:19:27 -0500

    commit

--- trace_log.2025/trace_log.start.app/run_trace.start.app.erl
+++ trace_log.2025/trace_log.start.app/run_trace.start.app.erl
@@ -9,11 +9,15 @@ FileName= string:concat(Path, File).
 file:delete(FileName).
 ListMod=[eh_aq_query_handler_api,eh_data_server,eh_dirty_read_query_handler_api,eh_event,eh_failure_detector_api,eh_failure_detector,eh_file_name,eh_node_state,eh_node_timestamp,eh_no_wait_query_handler_api,eh_persist_data,eh_persist_storage_data,eh_query_handler,eh_repl_data_manager_api,eh_repl_data_manager,eh_storage_data_api,eh_storage_data,eh_storage_data_operation_api,eh_system_server,eh_wait_query_handler_api,eh_write_conflict_resolver_api,eh_write_conflict_resolver,erlang_craq_validate].
 
-DbgList = lists:foldl(fun(X, Sum) -> [{X, '_', '_'}] ++ Sum end, [], ListMod).
+
+DbgList = lists:foldl(fun(X, Sum) -> [{X, '_', fun(_) -> return_trace() end}] ++ Sum end, [], ListMod).
 {ok, Dev} = file:open(FileName,[write]).
-recon_trace:calls(DbgList, 10000, [{scope, local}, {io_server, Dev},{args, arity}]).
+recon_trace:calls(DbgList, LogNum, [return_to, {scope, local}, {io_server, Dev}]).
 
 
+%DbgList = lists:foldl(fun(X, Sum) -> [{X, '_', '_'}] ++ Sum end, [], ListMod).
+%{ok, Dev} = file:open(FileName,[write]).
+%recon_trace:calls(DbgList, 10000, [{scope, local}, {io_server, Dev},{args, arity}]).
 %N1 = 'ec_n1@rabbitmq4-1'.
 %N2 = 'ec_n2@rabbitmq4-2'.
 %N3 = 'ec_n3@rabbitmq4-3'.
