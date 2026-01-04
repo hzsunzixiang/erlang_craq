754991a402b4e90223600e20b6ea41a75a8207f8
ericksun <haichengsun123@163.com>
2026-01-03 21:59:13 -0500

    add node

--- trace_log.2025/trace_log.add.node/run_trace.setup.repl.erl
+++ trace_log.2025/trace_log.add.node/run_trace.setup.repl.erl
@@ -1,8 +1,8 @@
 
 f().
-File="trace_log.add_node.node1.txt".
+%File="trace_log.add_node.node1.txt".
 %File="trace_log.add_node.node2.txt".
-%File="trace_log.add_node.node3.txt".
+File="trace_log.add_node.node3.txt".
 
 %% 加载所有的模块,这里加载的
 LP = fun() -> [code:ensure_loaded(list_to_atom(filename:rootname(filename:basename(F)))) || P <- code:get_path(), F <- filelib:wildcard(P ++ "/*.beam")] end.
@@ -10,7 +10,7 @@ LP().  %% 同步加载
 
 LogNum=50000.
 
-Path="/home/ericksun/program/erlang_craq/trace_log.2025/trace_log.add_node/".
+Path="/home/ericksun/program/erlang_craq/trace_log.2025/trace_log.add.node/".
 file:make_dir(FileName).
 FileName= string:concat(Path, File).
 file:delete(FileName).
